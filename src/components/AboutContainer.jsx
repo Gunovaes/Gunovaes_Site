9b9e2d767da5e86ea8a6adea17edeec4142ef3b0
GUSTAVO NOVAES PEDROSO <gunovaes16@gmail.com>
2024-01-30 12:24:55 -0300

    Update About.me

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -6,7 +6,7 @@ const AboutContainer = () => {
           Programador dedicado e com vontade de aprender que busca uma oportunidade de demonstrar seus valores, conhecimentos e habilidades dentro de um ambiente profissional e dinâmico.
         </p>
         <p>
-          Possuo conhecimentos em JavaScript Vanila, React, Angular, Node.js, Saass, Mongodb e Angular, além disso possuo conhecimento prévio em redes, manutenção de hardware e sistemas operacionais. Sou formado em Análise e Desenvolvimento de Sistemas pela Universidade Nove de Julho, também possuo formação técnica em administração, forrmação na qual tive oportunidade de aprender a desenvolver e analisar planilhas eletrônicas. Ainda não tive chance de adquirir experiência registrada na área o que me deixa ainda mais disposto a agarrar oportunidades e dar o meu melhor dentro de toda a minha jornada.  
+          Possuo conhecimentos em JavaScript Vanila, React, Angular, Node.js, Sass, Mongodb e Angular, além disso possuo conhecimento prévio em redes, manutenção de hardware e sistemas operacionais. Sou formado em Análise e Desenvolvimento de Sistemas pela Universidade Nove de Julho, também possuo formação técnica em administração, forrmação na qual tive oportunidade de aprender a desenvolver e analisar planilhas eletrônicas. Ainda não tive chance de adquirir experiência registrada na área, o que me deixa ainda mais disposto a agarrar oportunidades e dar o meu melhor dentro de toda a minha jornada.  
         </p>
     </section>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
             <SocialNetworks/>
             <InformationContainer/>
             <br/>
-            <a href='https://drive.google.com/file/d/15mMrxcz3UPd5QmhPS7wWBo5a1N44IubC/view?usp=drive_link' target="blank" className='btn'>
+            <a href='https://drive.google.com/file/d/1Y898DSWtajK-sUVK-ivUFH2ssekGws2B/view?usp=drive_link' target="blank" className='btn'>
                 Download Currículo (.pdf)    
             </a> 
         </aside>
